e20773d93e8772228b9c60488c5dd6428236a407
hamed <hamed.hafez1@yahoo.com>
2023-12-31 14:54:28 +0330

    update weather forecast api

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,14 +19,19 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    public async Task<string> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToArray();
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q=kish&appid=a9bc40e37b9e8727e004e89d553ecb84");
+        var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
     }
 }
