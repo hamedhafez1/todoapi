d24b5020dae3edf2cbb82114bf4dc55276cf11b0
hamed <hamed.hafez1@yahoo.com>
2024-01-01 15:00:25 +0330

    develop weather forecast

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,7 @@
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TodoApi.Models;
 
 namespace TodoApi.Controllers;
 
@@ -18,20 +21,22 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public async Task<string> Get()
+    [HttpGet("{cityName}")]
+    public async Task<ActionResult<WeatherForecast?>> GetWeatherForecast(string cityName)
     {
-        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        // {
-        //     Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //     TemperatureC = Random.Shared.Next(-20, 55),
-        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        // })
-        // .ToArray();
-        var client = new HttpClient();
-        var request = new HttpRequestMessage(HttpMethod.Get, "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q=kish&appid=a9bc40e37b9e8727e004e89d553ecb84");
-        var response = await client.SendAsync(request);
-        response.EnsureSuccessStatusCode();
-        return await response.Content.ReadAsStringAsync();
+        try
+        {
+            var client = new HttpClient();
+            // var request = new HttpRequestMessage(HttpMethod.Get, string.Format("https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q={0}&appid=a9bc40e37b9e8727e004e89d553ecb84", city));
+            var response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q={cityName}&appid=a9bc40e37b9e8727e004e89d553ecb84");
+            response.EnsureSuccessStatusCode();
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<WeatherForecast>(jsonString);
+        }
+        catch
+        {
+            throw;
+        }
+
     }
 }
