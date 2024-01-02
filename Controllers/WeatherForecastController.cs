5656ce9c5664b0bfb5699f2dddcadd5227005709
hamed <hamed.hafez1@yahoo.com>
2024-01-02 08:56:48 +0330

    retouch WeatherForecastController

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -9,33 +9,29 @@ namespace TodoApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
 
     [HttpGet("{cityName}")]
     public async Task<ActionResult<WeatherForecast?>> GetWeatherForecast(string cityName)
     {
         try
         {
-            var client = new HttpClient();
-            // var request = new HttpRequestMessage(HttpMethod.Get, string.Format("https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q={0}&appid=a9bc40e37b9e8727e004e89d553ecb84", city));
-            var response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q={cityName}&appid=a9bc40e37b9e8727e004e89d553ecb84");
-            response.EnsureSuccessStatusCode();
-            var jsonString = await response.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<WeatherForecast>(jsonString);
+            if (!string.IsNullOrEmpty(cityName))
+            {
+                var client = new HttpClient();
+                // var request = new HttpRequestMessage(HttpMethod.Get, string.Format("https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q={0}&appid=a9bc40e37b9e8727e004e89d553ecb84", city));
+                var response = await client.GetAsync($"https://api.openweathermap.org/data/2.5/weather?units=metric&lang=fa&q={cityName}&appid=a9bc40e37b9e8727e004e89d553ecb84");
+                response.EnsureSuccessStatusCode();
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<WeatherForecast>(jsonString);
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
         catch
         {
-            throw;
+            return StatusCode(500);
         }
 
     }
